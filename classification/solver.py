bb299ef15096bf75a7d2494e077d0769626c577d
wayne1029jihad <wayne1029jihad@gmail.com>
2018-05-01 16:51:53 +0800

    :D

---
+++ classification/solver.py
@@ -0,0 +1,16 @@
+import sys
+# import anything else you need
+
+
+if len(sys.argv) < 3:
+    print 'usage: python solver.py <training_data> <testing_data>'
+# you can add other arguments after <training_data> <testing_data>
+
+# load data
+
+# train your model
+
+# print train loss and test accuracy after every epoch
+
+# save weight
+
---
+++ classification/tester.py
@@ -0,0 +1,14 @@
+import sys
+# import anything else you need
+
+if len(sys.argv) < 3:
+    print 'usage: python solver.py <trained_weight> <data>'
+# you can add other arguments after <trained_weight> <data>
+
+# load data
+
+# load trained model
+
+# generate model output 
+
+
